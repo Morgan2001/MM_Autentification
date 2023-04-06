5b7c4b9e7441f7e06c8c85cd59189c34a142d556
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-04-06 17:54:50 +0300

    Implemented PasswordHasher

--- src/Authentication.Infrastructure/Services/PasswordHasher.cs
+++ src/Authentication.Infrastructure/Services/PasswordHasher.cs
@@ -1,16 +1,33 @@
-﻿using Authentication.Application.Interfaces;
+﻿using System.Security.Cryptography;
+using System.Text;
+using Authentication.Application.Interfaces;
 
 namespace Authentication.Infrastructure.Services;
 
 public class PasswordHasher : IPasswordHasher
 {
+    private const int KeySize = 64;
+    private const int Iterations = 350000;
+    private readonly HashAlgorithmName _hashAlgorithm = HashAlgorithmName.SHA512;
+
     public string HashPassword(string password, out byte[] salt)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException(password);
+
+        salt = RandomNumberGenerator.GetBytes(KeySize);
+        byte[] hash = Rfc2898DeriveBytes.Pbkdf2(
+            Encoding.UTF8.GetBytes(password),
+            salt,
+            Iterations,
+            _hashAlgorithm,
+            KeySize);
+
+        return Convert.ToHexString(hash);
     }
 
     public bool VerifyPassword(string password, string hash, byte[] salt)
     {
-        throw new NotImplementedException();
+        byte[] hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, _hashAlgorithm, KeySize);
+        return hashToCompare.SequenceEqual(Convert.FromHexString(hash));
     }
 }
